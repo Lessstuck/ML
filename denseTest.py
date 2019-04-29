a4f192b8706621134e2786592d7ef9a1374fc81d
Les Stuck <les@lesstuck.com>
2019-04-28 21:00:16 -0600

    added missing matplotlib.pyplot definitions

--- denseTest.py
+++ denseTest.py
@@ -9,6 +9,38 @@ for test_images, test_labels in test_dataset.take(1):
   test_labels = test_labels.numpy()
   predictions = model.predict(test_images)
 
+# define plotting functions
+def plot_image(i, predictions_array, true_labels, images):
+  predictions_array, true_label, img = predictions_array[i], true_labels[i], images[i]
+  plt.grid(False)
+  plt.xticks([])
+  plt.yticks([])
+
+  plt.imshow(img[...,0], cmap=plt.cm.binary)
+
+  predicted_label = np.argmax(predictions_array)
+  if predicted_label == true_label:
+    color = 'blue'
+  else:
+    color = 'red'
+
+  plt.xlabel("{} {:2.0f}% ({})".format(class_names[predicted_label],
+                                100*np.max(predictions_array),
+                                class_names[true_label]),
+                                color=color)
+
+def plot_value_array(i, predictions_array, true_label):
+  predictions_array, true_label = predictions_array[i], true_label[i]
+  plt.grid(False)
+  plt.xticks([])
+  plt.yticks([])
+  thisplot = plt.bar(range(10), predictions_array, color="#777777")
+  plt.ylim([0, 1])
+  predicted_label = np.argmax(predictions_array)
+
+  thisplot[predicted_label].set_color('red')
+  thisplot[true_label].set_color('blue')
+
 
 # display results for first 15 test test images
 # Plot the first X test images, their predicted label, and the true label
